8792854f4a52e42f4592a1836730525a7ff5ac37
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-10-03 21:47:55 +0200

    blehhh

---
+++ RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,36 @@
+﻿
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+
+        //HashSet<int> set = new HashSet<int>();
+        //set = nums.ToHashSet();
+        //var tmp = set.ToArray();
+
+        //for (int i = 0; i < tmp.Length; i++) nums[i] = tmp[i];
+        //int k = tmp.Length;
+        //return k;
+
+        //more optimized with linq:
+        //var unique = nums.Distinct().ToArray(); 
+        //Array.Copy(unique, nums, unique.Length);
+        //return unique.Length;
+        //nvm it was worse
+
+        //surely
+        int i = 0; 
+        for (int j = 1; j < nums.Length; j++)
+        {
+            if (nums[j] != nums[i])
+            {
+                i++;
+                nums[i] = nums[j]; 
+            }
+        }
+        return i + 1;
+        //0ms runtime gg
+    }
+}
+
+

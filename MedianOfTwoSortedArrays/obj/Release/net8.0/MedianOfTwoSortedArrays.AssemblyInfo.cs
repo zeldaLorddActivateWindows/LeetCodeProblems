603d2202b913ff91ce0b55b4316787784dc8e93f
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-10-03 20:42:17 +0200

    i am very retarded and should end my life

--- MedianOfTwoSortedArrays/obj/Release/net8.0/MedianOfTwoSortedArrays.AssemblyInfo.cs
+++ MedianOfTwoSortedArrays/obj/Release/net8.0/MedianOfTwoSortedArrays.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedianOfTwoSortedArrays")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a629aacac2705987cb4dd61c9fa1924083a257")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedianOfTwoSortedArrays")]
---
+++ RegularExpressionMatching/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Channels;
+Solution solution = new Solution();
+
+public class Solution
+{
+    public bool IsMatch(string s, string p)
+    {
+        //    var oldP = p;
+        //    if (!Regex.IsMatch(s, pattern: "^[a-z]+$")) return false;
+        //    if (!Regex.IsMatch(p, pattern: "^[a-z.*]+$")) return false;
+        //    if (p.Contains('.'))
+        //    {
+
+        //        int i = p.IndexOf('.');
+        //        if (i != -1) 
+        //        {
+        //            p = p.Substring(0, i) + s[i] + p.Substring(i + 1);
+        //        }
+        //    }
+        //    if (p.Contains('*'))
+        //    {
+
+        //        int i = p.IndexOf('*');
+        //        if (i != -1)
+        //        {
+        //            if (oldP[i - 1] == '.')  return true;
+        //            if ()
+        //            p = p.Substring(0, i) + s[i-1] + p.Substring(i + 1);
+        //        }
+        //    }
+        //    if(p == s) return true;
+        //    return false;
+        return Regex.IsMatch(s, "^" + p + "$");
+    }
+}

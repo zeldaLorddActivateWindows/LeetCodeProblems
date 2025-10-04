674ec8cfbca1fc04f8fff1339427de93b0750631
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-10-04 10:10:38 +0200

    pex

---
+++ ContainerWithMostWater/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+Solution sln = new Solution();
+Console.WriteLine(sln.MaxArea(height));
+
+
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int i = 0, j = height.Length - 1;
+        int maxArea=0;
+        while (i < j)
+        {
+            int area = Math.Min(height[i], height[j]) * (j - i);
+            maxArea = Math.Max(maxArea, area);
+            if (height[i] < height[j]) i++;
+            else j--;
+        }
+        return maxArea;
+    }
+}
+

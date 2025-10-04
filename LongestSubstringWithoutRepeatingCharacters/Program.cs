4d00e72109b1c5d54b7c6b07cf79bfd3b3752dcb
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-10-04 22:22:41 +0200

    type shiiii

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return 0;
+
+        int maxLength = 0;
+        int start = 0;
+        Dictionary<char, int> charIndexMap = new Dictionary<char, int>();
+
+        for (int end = 0; end < s.Length; end++)
+        {
+            if (charIndexMap.ContainsKey(s[end]))
+            {
+                start = Math.Max(start, charIndexMap[s[end]] + 1);
+            }
+
+            charIndexMap[s[end]] = end;
+            maxLength = Math.Max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+}
+
+//public class Solution
+//{
+//    public unsafe int LengthOfLongestSubstring(string s)
+//    {
+//        int* lastSeen = stackalloc int[128];
+//        int maxLen = 0, start = 0;
+
+//        for (int i = 0; i < 128; i++) lastSeen[i] = 0; <-- nerd solution
+
+//        fixed (char* p = s)
+//        {
+//            for (int end = 0; end < s.Length; end++)
+//            {
+//                start = Math.Max(start, lastSeen[p[end]]);
+//                maxLen = Math.Max(maxLen, end - start + 1);
+//                lastSeen[p[end]] = end + 1;
+//            }
+//            return maxLen;
+//        }
+//    }
+//}

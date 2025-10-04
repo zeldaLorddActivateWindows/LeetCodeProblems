b150f1fa22871ef23d0fe534f814c8e1200958ba
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-10-04 10:22:56 +0200

    beer game

---
+++ WaterBottles/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Solution sln = new Solution();
+sln.NumWaterBottles(9, 3);
+
+public class Solution
+{
+    public int NumWaterBottles(int numBottles, int numExchange)
+    {
+        int totalDrank = numBottles;
+        int empty = numBottles;
+
+        while (empty >= numExchange)
+        {
+            int newBottles = empty / numExchange; 
+            int remainder = empty % numExchange; 
+            totalDrank += newBottles;
+            empty = newBottles + remainder;
+        }
+
+        return totalDrank;
+    }
+}

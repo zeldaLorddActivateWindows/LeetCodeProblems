4518d2c31421047990c7f69c61845570304ed7d6
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2025-10-05 09:10:40 +0200

    i am the imposter

---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+int x = 123;
+var sln = new Solution();
+Console.WriteLine(x);
+Console.WriteLine(sln.Reverse(120));
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        if (x is >= int.MaxValue or <= int.MinValue) return 0;
+
+        bool isNegative = x < 0;
+        string xStr = Math.Abs(x).ToString();
+        char[] resultStr = new char[xStr.Length];
+        for (int i = 0; i < xStr.Length; i++) resultStr[(resultStr.Length - 1) - i] = xStr[i];
+        if (!int.TryParse(new string(resultStr), out int reversed)) return 0;
+        return isNegative ? -reversed : reversed;
+    }
+}
